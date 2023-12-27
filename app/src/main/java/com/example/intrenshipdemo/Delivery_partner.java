7b7d284d5e72f0857fae4a8ad9c7eac091e5d416
Atharva <atharva4b@gmail.coim>
2023-12-27 12:57:23 +0530

    Hiee Update From Ab 271222 Without Location

---
+++ app/src/main/java/com/example/intrenshipdemo/Delivery_partner.java
@@ -0,0 +1,13 @@
+package com.example.intrenshipdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Delivery_partner extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delivery_partner);
+    }
+}
